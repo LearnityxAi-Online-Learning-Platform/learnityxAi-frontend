352e0086966f06955973dbb51ef7c2364415f905
K.Chathura Priyadasun <cpriyadasun@gmail.com>
2025-11-04 10:00:20 +0530

    implement profile page

---
+++ src/components/UserComponents/UserCourses.tsx
@@ -0,0 +1,7 @@
+export default function UserCourses() {
+    return (
+        <div>
+
+        </div>
+    )
+}
---
+++ src/components/UserComponents/UserRatings.tsx
@@ -0,0 +1,7 @@
+export default function UserRatings() {
+    return (
+        <div>
+
+        </div>
+    )
+}
