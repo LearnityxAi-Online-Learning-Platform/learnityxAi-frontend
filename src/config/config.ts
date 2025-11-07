869456c224766e5fbf3e0377ec3dc5dcbd011f4d
K.Chathura Priyadasun <cpriyadasun@gmail.com>
2025-11-08 00:22:00 +0530

    Update all couse page filters

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,3 +2,6 @@
 
 export const base_url_frontend = "http://localhost:3000";
 export const base_url = "http://localhost:5000";
+
+// export const base_url_frontend = "https://www.learnityxai.corespace.click";
+// export const base_url = "https://www.learnityxai.corespace.click/";
